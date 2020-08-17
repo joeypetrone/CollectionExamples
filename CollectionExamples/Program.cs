ea91e88b89410e026d6f0d59773abe089e5c18ca
Joey Petrone <joey.petrone@gmail.com>
2020-08-17 18:17:44 -0500

    Add project files.

---
+++ CollectionExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
