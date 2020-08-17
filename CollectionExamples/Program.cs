e8ea4eee00be091a17f0a8a13f4c74ce1793c7b1
Joey Petrone <joey.petrone@gmail.com>
2020-08-17 18:50:33 -0500

    List Add and AddRange

--- CollectionExamples/Program.cs
+++ CollectionExamples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CollectionExamples
 {
@@ -6,7 +7,35 @@ namespace CollectionExamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var instructors = new List<string>();
+            var students = new List<string>();
+            var evening11 = new List<string>();
+
+            var numbers = new List<int>();
+
+            instructors.Add("Jameka");
+            instructors.Add("Nathan");
+            instructors.Add("Dylan");
+
+            numbers.Add(1);
+            numbers.Add(3);
+            numbers.Add(5);
+
+            students.Add("Aaron");
+            students.Add("John");
+            students.Add("Monique");
+
+            //add multiple at once from an existing list
+            evening11.AddRange(instructors);
+            evening11.AddRange(students);
+
+            foreach (var person in evening11)
+            {
+                Console.WriteLine($"{person} is in evening cohort 11.");
+            }
+
+
+
         }
     }
 }
